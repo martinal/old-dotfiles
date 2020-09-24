df571731ecae57f215e4e6783937d03cc070d8dc
Martin Sandve Alnæs <martin.alnes@vipps.no>
2020-09-24 14:24:00 +0200

    Add i3 bins and symlinking script.

---
+++ README.md
@@ -0,0 +1,1 @@
+Martins config files.
---
+++ makesymlinks.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir $HOME/.config/i3
+mkdir $HOME/.config/nvim
+ln -s $HOME/dotfiles/i3/config $HOME/.config/i3/config
+ln -s $HOME/dotfiles/ideavim/ideavimrc $HOME/.ideavimrc
+ln -s $HOME/dotfiles/inputrc/inputrc $HOME/.inputrc
+ln -s $HOME/dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim
