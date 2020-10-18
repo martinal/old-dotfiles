dcb7b05f47bcfd4e38e172eab6dcf6674f680f10
Martin Sandve Alnæs <martin.alnes@gmail.com>
2020-10-18 23:43:19 +0200

    Fontsize parameter.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 mkdir $HOME/.config/i3
-mkdir $HOME/.config/nvim
 ln -s $HOME/dotfiles/i3/config $HOME/.config/i3/config
+
+mkdir $HOME/.config/i3status
 ln -s $HOME/dotfiles/i3status/config $HOME/.config/i3status/config
+
+mkdir $HOME/.config/nvim
+ln -s $HOME/dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim
+
 ln -s $HOME/dotfiles/ideavim/ideavimrc $HOME/.ideavimrc
 ln -s $HOME/dotfiles/inputrc/inputrc $HOME/.inputrc
-ln -s $HOME/dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim
